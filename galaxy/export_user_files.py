0c0ef48eddeacdae21ad3d3e7606e098a6230bc5
Bjoern Gruening <bjoern.gruening@gmail.com>
2014-09-23 23:22:28 +0200

    Make use of the new ~/config folder and copy a recent verison of ~/config to /export/.distribution_config/; This should make it possible to update already existing installations.

--- galaxy/export_user_files.py
+++ galaxy/export_user_files.py
@@ -3,8 +3,11 @@ import os
 import shutil
 import subprocess
 
-PG_DATA_DIR_DEFAULT = sys.argv[1] or "/var/lib/postgresql/9.3/main"
-PG_DATA_DIR_HOST = "/export/postgresql/9.3/main/"
+if len( sys.argv ) == 2:
+    PG_DATA_DIR_DEFAULT = sys.argv[1]
+else:
+    PG_DATA_DIR_DEFAULT = "/var/lib/postgresql/9.3/main"
+PG_DATA_DIR_HOST = os.environ.get("PG_DATA_DIR_HOST", "/export/postgresql/9.3/main/")
 PG_CONF = '/etc/postgresql/9.3/main/postgresql.conf'
 
 
@@ -38,35 +41,32 @@ if __name__ == "__main__":
         If the user re-starts (with docker start) the container the file /.galaxy_save is found and the linking
         is aborted.
     """
-    if os.path.exists('/export/') and not os.path.exists('/.galaxy_save'):
-        change_path('/galaxy-central/universe_wsgi.ini')
-        change_path('/galaxy-central/database/files/')
-        change_path('/galaxy-central/database/job_working_directory/')
-        change_path('/galaxy-central/tool_deps/')
-        change_path('/galaxy-central/tool-data/')
-        change_path('/shed_tools/')
-        change_path('/galaxy-central/job_conf.xml')
-        change_path('/galaxy-central/shed_tool_conf.xml')
-        change_path('/galaxy-central/shed_data_manager_conf.xml')
-        change_path('/galaxy-central/integrated_tool_panel.xml')
+    if os.path.exists( '/export/.distribution_config/' ):
+        shutil.rmtree( '/export/.distribution_config/' )
+    shutil.copytree( '/galaxy-central/config/', '/export/.distribution_config/' )
+    change_path('/galaxy-central/config/')
+    change_path('/galaxy-central/database/files/')
+    change_path('/galaxy-central/database/job_working_directory/')
+    change_path('/galaxy-central/tool_deps/')
+    change_path('/galaxy-central/tool-data/')
+    change_path('/shed_tools/')
 
-        if not os.path.exists( PG_DATA_DIR_HOST ) or 'PG_VERSION' not in os.listdir( PG_DATA_DIR_HOST ):
-            dest_dir = os.path.dirname( PG_DATA_DIR_HOST )
-            if not os.path.exists( dest_dir ):
-                os.makedirs(dest_dir)
-            # User given dbpath, usually a directory from the host machine
-            # copy the postgresql data folder to the new location
-            subprocess.call('cp -R %s/* %s' % (PG_DATA_DIR_DEFAULT, PG_DATA_DIR_HOST), shell=True)
-            # copytree needs an non-existing dst dir, how annoying :(
-            #shutil.copytree(PG_DATA_DIR_DEFAULT, PG_DATA_DIR_HOST)
-            subprocess.call('chown -R postgres:postgres /export/postgresql/', shell=True)
-            subprocess.call('chmod -R 0755 /export/', shell=True)
-            subprocess.call('chmod -R 0700 %s' % PG_DATA_DIR_HOST, shell=True)
-        # change data_directory of PostgreSQL to the new location
-        new_data_directory = "'%s'" % PG_DATA_DIR_HOST
-        cmd = 'sed -i "s|data_directory = .*|data_directory = %s|g" %s' % (new_data_directory, PG_CONF)
-        subprocess.call(cmd, shell=True)
+    if not os.path.exists( PG_DATA_DIR_HOST ) or 'PG_VERSION' not in os.listdir( PG_DATA_DIR_HOST ):
+        dest_dir = os.path.dirname( PG_DATA_DIR_HOST )
+        if not os.path.exists( dest_dir ):
+            os.makedirs(dest_dir)
+        # User given dbpath, usually a directory from the host machine
+        # copy the postgresql data folder to the new location
+        subprocess.call('cp -R %s/* %s' % (PG_DATA_DIR_DEFAULT, PG_DATA_DIR_HOST), shell=True)
+        # copytree needs an non-existing dst dir, how annoying :(
+        #shutil.copytree(PG_DATA_DIR_DEFAULT, PG_DATA_DIR_HOST)
+        subprocess.call('chown -R postgres:postgres /export/postgresql/', shell=True)
+        subprocess.call('chmod -R 0755 /export/', shell=True)
+        subprocess.call('chmod -R 0700 %s' % PG_DATA_DIR_HOST, shell=True)
 
-        # mark user files as exported
-        open('/.galaxy_save', 'a').close()
+    # change data_directory of PostgreSQL to the new location
+    # This is not strictly needed because we are starting postgresql with pointing to the data directory
+    new_data_directory = "'%s'" % PG_DATA_DIR_HOST
+    cmd = 'sed -i "s|data_directory = .*|data_directory = %s|g" %s' % (new_data_directory, PG_CONF)
+    subprocess.call(cmd, shell=True)
 
