9a64e04fe4e53cd09e2103b343b44245f78a67f8
Bjoern Gruening <bjoern.gruening@gmail.com>
2014-09-28 14:22:15 +0200

    Run supervisor as daemon and track the logfiles instead.

--- galaxy/startup.sh
+++ galaxy/startup.sh
@@ -5,5 +5,6 @@ cd /galaxy-central/
 # symlinks will point from the original location to the new path under /export/
 # If /export/ is not given, nothing will happen in that step
 python ./export_user_files.py $PG_DATA_DIR_DEFAULT
-
 /usr/bin/supervisord
+sleep 5
+tail -f /root/*.log
