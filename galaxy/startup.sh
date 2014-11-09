0b7ab7c39b6de780ce3a976144dec5e828c8a9df
Bjoern Gruening <bjoern.gruening@gmail.com>
2014-11-09 21:09:39 +0100

    unmount /var/lib/docker before exporting the directory

--- galaxy/startup.sh
+++ galaxy/startup.sh
@@ -4,6 +4,7 @@ cd /galaxy-central/
 # If /export/ is mounted, export_user_files file moving all data to /export/
 # symlinks will point from the original location to the new path under /export/
 # If /export/ is not given, nothing will happen in that step
+umount /var/lib/docker
 python ./export_user_files.py $PG_DATA_DIR_DEFAULT
 
 # Configure SLURM with runtime hostname.
