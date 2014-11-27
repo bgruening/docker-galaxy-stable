37042034bd7d1280a44781009846f1a235561f7b
Bjoern Gruening <bjoern.gruening@gmail.com>
2014-11-28 00:22:54 +0100

    change start and stop of Galaxy into the install_repo_script!

--- galaxy/install_repo_wrapper.sh
+++ galaxy/install_repo_wrapper.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # start Galaxy
-/usr/bin/supervisord
-
+/etc/init.d/postgresql start
+./run.sh --daemon
 sleep 60
+
 python ./scripts/api/install_tool_shed_repositories.py --api admin -l http://localhost:8080 --tool-deps --repository-deps $1
 exit_code=$?
 
@@ -12,4 +13,5 @@ if [ $exit_code != 0 ] ; then
 fi
 
 # stop everything
-supervisorctl stop all
+./run.sh --stop-daemon
+service postgresql stop
