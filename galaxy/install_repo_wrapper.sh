239708c007892eceeda92089033167cd17c30e40
Bjoern Gruening <bjoern.gruening@gmail.com>
2014-11-27 09:10:21 +0100

    Add wrapper script to make it easier to install repositories from the command line. This will be used by child containers.

---
+++ galaxy/install_repo_wrapper.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# start Galaxy
+/usr/bin/supervisord
+
+sleep 60
+python ./scripts/api/install_tool_shed_repositories.py --api admin -l http://localhost:8080 --tool-deps --repository-deps $1
+exit_code=$?
+
+if [ $exit_code != 0 ] ; then
+    exit $exit_code
+fi
+
+# stop everything
+supervisorctl stop all
