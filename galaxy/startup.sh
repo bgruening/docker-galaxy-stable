ed58bf20f74a21cc5d14383f502876374d7c4a69
John Chilton <jmchilton@gmail.com>
2015-01-31 22:26:59 -0500

    Do not write helper scripts to /galaxy-central.
    Write them to /usr/bin/local instead.

--- galaxy/startup.sh
+++ galaxy/startup.sh
@@ -5,7 +5,7 @@ cd /galaxy-central/
 # symlinks will point from the original location to the new path under /export/
 # If /export/ is not given, nothing will happen in that step
 umount /var/lib/docker
-python ./export_user_files.py $PG_DATA_DIR_DEFAULT
+python /usr/local/bin/export_user_files.py $PG_DATA_DIR_DEFAULT
 
 # Configure SLURM with runtime hostname.
 python /usr/sbin/configure_slurm.py
