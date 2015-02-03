394753be7c2bf7ab7bf209dd96e9e8aa4fec2ab9
Bjoern Gruening <bjoern.gruening@gmail.com>
2015-02-03 17:23:25 +0100

    improve installation wrapper

--- galaxy/install_repo_wrapper.sh
+++ galaxy/install_repo_wrapper.sh
@@ -8,7 +8,7 @@ sudo -E -u galaxy ./run.sh --daemon --log-file=$install_log --pid-file=galaxy_in
 galaxy_install_pid=`cat galaxy_install.pid`
 
 while : ; do
-    tail -n 2 $install_log | grep -q "Removing PID file galaxy_install.pid"
+    tail -n 2 $install_log | grep -E -q "Removing PID file galaxy_install.pid|Daemon is already running"
     if [ $? -eq 0 ] ; then
         echo "Galaxy could not be started."
         echo "More information about this failure may be found in the following log snippet from galaxy_install.log:"
@@ -37,6 +37,6 @@ if [ $exit_code != 0 ] ; then
 fi
 
 # stop everything
-./run.sh --stop-daemon
+sudo -E -u galaxy ./run.sh --stop-daemon --log-file=$install_log --pid-file=galaxy_install.pid
 rm $install_log
 service postgresql stop
