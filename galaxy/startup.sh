5bd8e589a73128a79fe9ba76f0bf75756dd8b51e
Bjoern Gruening <bjoern.gruening@gmail.com>
2015-03-07 00:10:41 +0100

    * restructure startup script * introduce NONUSE to specify services that we don't want to start (nodejs, reports, proftp) * add more verbose output and add some documentation

--- galaxy/startup.sh
+++ galaxy/startup.sh
@@ -7,33 +7,55 @@ cd /galaxy-central/
 umount /var/lib/docker
 python /usr/local/bin/export_user_files.py $PG_DATA_DIR_DEFAULT
 
+
 # Configure SLURM with runtime hostname.
 python /usr/sbin/configure_slurm.py
+
+# $NONUSE can be set to include proftp, reports or nodejs
+# if included we will _not_ start these services.
+function start_supersisor {
+    /usr/bin/supervisord
+    sleep 5
+    if [[ $NONUSE != *"proftp"* ]]
+    then
+        echo "Starting ProFTP"
+        supervisorctl start proftpd
+    fi
+    if [[ $NONUSE != *"reports"* ]]
+    then
+        echo "Starting Galaxy reports webapp"
+        supervisorctl start reports
+    fi
+    if [[ $NONUSE != *"nodejs"* ]]
+    then
+        echo "Starting nodejs"
+        supervisorctl start galaxy:galaxy_nodejs_proxy
+    fi
+}
+
 
 # Try to guess if we are running under --privileged mode
 if mount | grep "/proc/kcore"; then
     echo "Disable Galaxy Interactive Environments. Start with --privileged to enable IE's."
     export GALAXY_CONFIG_INTERACTIVE_ENVIRONMENT_PLUGINS_DIRECTORY=""
-    /usr/bin/supervisord
-    sleep 5
+    start_supersisor
 else
     echo "Enable Galaxy Interactive Environments."
     export GALAXY_CONFIG_INTERACTIVE_ENVIRONMENT_PLUGINS_DIRECTORY="config/plugins/interactive_environments"
     if [ x$DOCKER_PARENT == "x" ]; then 
         #build the docker in docker environment
         bash /root/cgroupfs_mount.sh
-        /usr/bin/supervisord
-        sleep 5
+        start_supersisor
         supervisorctl start docker
     else
         #inheriting /var/run/docker.sock from parent, assume that you need to
         #run docker with sudo to validate
         echo "galaxy ALL = NOPASSWD : ALL" >> /etc/sudoers
-        /usr/bin/supervisord
-        sleep 5
+        start_supersisor
     fi
 fi
 
+# Enable verbose output
 if [ `echo ${GALAXY_LOGGING:-'no'} | tr [:upper:] [:lower:]` = "full" ]
     then
         tail -f /var/log/supervisor/* /var/log/nginx/* /home/galaxy/*.log
@@ -41,9 +63,11 @@ if [ `echo ${GALAXY_LOGGING:-'no'} | tr [:upper:] [:lower:]` = "full" ]
         tail -f /home/galaxy/*.log
 fi
 
+# Disable authentication of Galaxy reports
 if [ "x$DISABLE_REPORTS_AUTH" != "x" ]
     then
         # disable authentification by deleting the htpasswd file
+        echo "Disable Galaxy reports authentification "
         rm /etc/nginx/htpasswd
 fi
 
