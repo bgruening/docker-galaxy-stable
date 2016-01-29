19baf85c624caa4f1cc9b00751d4e66dc3b795b6
Cameron Jack <cameron.jack@anu.edu.au>
2016-01-29 14:08:10 +1100

    scripts to import a workflow from .ga file into galaxy. Added pip install bioblend to satisfy python dependency

---
+++ galaxy/install_workflow.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import argparse
+from bioblend import galaxy
+
+def main():
+    """
+        This script uses bioblend to import .ga workflow files into a running instance of Galaxy
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('workflow_path', help='The path to the workflow file')
+    args = parser.parse_args()
+    
+    gi = galaxy.GalaxyInstance(url='http://127.0.0.1:8080', email='admin@galaxy.org', password='admin')
+    gi.workflows.import_workflow_from_local_path(args.workflow_path)
+
+if __name__ == '__main__':
+    main()
