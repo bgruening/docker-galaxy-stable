95f6858b02d37559ccae84fe55abe98717617f0f
Cameron Jack <cameron.jack@anu.edu.au>
2016-02-03 15:48:25 +1100

    Checks the existing Galaxy workflow UUIDs first to avoid duplicate installs

--- galaxy/install_workflow.py
+++ galaxy/install_workflow.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import argparse
 from bioblend import galaxy
+import json
+import os
 
 def main():
     """
@@ -10,8 +12,11 @@ def main():
     parser.add_argument('workflow_path', help='The path to the workflow file')
     args = parser.parse_args()
     
+    import_uuid = json.load(open(args.workflow_path, 'r'))['uuid']
     gi = galaxy.GalaxyInstance(url='http://127.0.0.1:8080', email='admin@galaxy.org', password='admin')
-    gi.workflows.import_workflow_from_local_path(args.workflow_path)
+    existing_uuids = [d['latest_workflow_uuid'] for d in gi.workflows.get_workflows()]
+    if import_uuid not in existing_uuids:
+        gi.workflows.import_workflow_from_local_path(args.workflow_path)
 
 if __name__ == '__main__':
     main()
