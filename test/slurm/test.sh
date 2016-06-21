ba810fd9e1dd8f02c4221d66d19d7cb58a287103
Marius van den Beek <m.vandenbeek@gmail.com>
2016-06-21 09:12:35 +0000

    Start container, wait for /export/galaxy-central/config creation, copy job_conf.xml, then test

--- test/slurm/test.sh
+++ test/slurm/test.sh
@@ -11,11 +11,18 @@ docker build -t slurm .
 docker run -d -v "$EXPORT":/export --name slurm \
            --hostname slurm \
            slurm
-# We copy the job_conf.xml to the $EXPORT folder
-mkdir -p "$EXPORT"/galaxy-central/config/
-cp job_conf.xml "$EXPORT"/galaxy-central/config/
 # We start galaxy (without the internal slurm, but with a modified job_conf.xml)
 # and link it to the slurm container (so that galaxy resolves the slurm container's hostname)
+docker run -d -e "NONUSE=slurmd,slurmctld" \
+   --link slurm --name galaxy-slurm-test -h galaxy \
+   -p 80:80 -v "$EXPORT":/export quay.io/bgruening/galaxy
+# We wait for the creation of the /galaxy-central/config/ if it does not exist yet
+sleep 20s
+# We copy the job_conf.xml to the $EXPORT folder
+cp job_conf.xml "$EXPORT"/galaxy-central/config/
+# We restart galaxy
+docker stop galaxy-slurm-test
+docker rm galaxy-slurm-test
 docker run -d -e "NONUSE=slurmd,slurmctld" \
    --link slurm --name galaxy-slurm-test -h galaxy \
    -p 80:80 -v "$EXPORT":/export quay.io/bgruening/galaxy
@@ -23,7 +30,7 @@ docker run -d -e "NONUSE=slurmd,slurmctld" \
 sleep 40s
 docker cp testscript.sh galaxy-slurm-test:/export/galaxy-central/testscript.sh
 docker exec galaxy-slurm-test chmod +x /export/galaxy-central/testscript.sh
-docker exec galaxy-slurm-test su - galaxy -c 'srun /export/galaxy-central/testscript.sh' | grep "SLURMD_NODENAME=slurm" && \
+docker exec galaxy-slurm-test su - galaxy -c 'srun hostname' | grep slurm && \
 docker stop galaxy-slurm-test slurm && \
 docker rm galaxy-slurm-test slurm
 # TODO: Run a galaxy tool and check it runs on the cluster
