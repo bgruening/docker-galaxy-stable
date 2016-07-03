7d56c26c5bf1b3109a90da45dd148a4988e3a12d
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-07-03 17:37:29 +0200

    create symlink for the ansible config changes to postgresql

--- galaxy/export_user_files.py
+++ galaxy/export_user_files.py
@@ -8,8 +8,6 @@ if len( sys.argv ) == 2:
 else:
     PG_DATA_DIR_DEFAULT = "/var/lib/postgresql/9.3/main"
 PG_DATA_DIR_HOST = os.environ.get("PG_DATA_DIR_HOST", "/export/postgresql/9.3/main/")
-PG_CONF = '/etc/postgresql/9.3/main/postgresql.conf'
-
 
 def change_path( src ):
     """
@@ -109,14 +107,10 @@ if __name__ == "__main__":
         # User given dbpath, usually a directory from the host machine
         # copy the postgresql data folder to the new location
         subprocess.call('cp -R %s/* %s' % (PG_DATA_DIR_DEFAULT, PG_DATA_DIR_HOST), shell=True)
+        os.symlink( os.path.join(os.environ.get('PG_CONF_DIR_DEFAULT'), 'conf.d'), os.path.join(PG_DATA_DIR_HOST, 'conf.d') )
         # copytree needs an non-existing dst dir, how annoying :(
         # shutil.copytree(PG_DATA_DIR_DEFAULT, PG_DATA_DIR_HOST)
         subprocess.call('chown -R postgres:postgres /export/postgresql/', shell=True)
         subprocess.call('chmod -R 0755 /export/', shell=True)
         subprocess.call('chmod -R 0700 %s' % PG_DATA_DIR_HOST, shell=True)
 
-    # change data_directory of PostgreSQL to the new location
-    # This is not strictly needed because we are starting postgresql with pointing to the data directory
-    new_data_directory = "'%s'" % PG_DATA_DIR_HOST
-    cmd = 'sed -i "s|data_directory = .*|data_directory = %s|g" %s' % (new_data_directory, PG_CONF)
-    subprocess.call(cmd, shell=True)
