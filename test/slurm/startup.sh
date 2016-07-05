3e685d1df7b5d46604f4a5b76d4d7ccd2f54618a
Marius van den Beek <m.vandenbeek@gmail.com>
2016-07-05 18:22:26 +0200

    Install docker in slurm container ...
    and mark /var/lib/docker as volume for docker-in-docker use.
    Also instruct slurm to load slurm.conf and munge.key directly
    from /export directory (the file location can be controlled with
    `-e SLURM_CONF_PATH=path/to/slurm.conf` and `MUNGE_KEY_PATH=path/to/munge.key`).

--- test/slurm/startup.sh
+++ test/slurm/startup.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
 # Setup the galaxy user UID/GID and pass control on to supervisor
+if id "$SLURM_USER_NAME" >/dev/null 2>&1; then
+        echo "user exists"
+else
+        echo "user does not exist, creating"
+        useradd -m -d /var/"$SLURM_USER_NAME" "$SLURM_USER_NAME"
+fi
 usermod -u $SLURM_UID  $SLURM_USER_NAME
 groupmod -g $SLURM_GID $SLURM_USER_NAME
-if [ -f /export/munge.key ]
-  then cp /export/munge.key /etc/munge/munge.key
-else
-  cp /etc/munge/munge.key /export/munge.key
+if [ ! -f "$MUNGE_KEY_PATH" ]
+  then
+    cp /etc/munge/munge.key "$MUNGE_KEY_PATH"
 fi
 
-if [ -f /export/slurm.conf ]
-  then cp /export/slurm.conf /etc/slurm-llnl/slurm.conf
-else
-  python /usr/local/bin/configure_slurm.py
-  cp /etc/slurm-llnl/slurm.conf /export/slurm.conf
+if [ ! -f "$SLURM_CONF_PATH" ]
+  then
+    python /usr/local/bin/configure_slurm.py
+    cp /etc/slurm-llnl/slurm.conf "$SLURM_CONF_PATH"
 fi
 if [ ! -f "$GALAXY_DIR"/.venv ]
   then
@@ -26,5 +30,5 @@ if [ ! -f "$GALAXY_DIR"/.venv ]
 fi
 chown $SLURM_USER_NAME /tmp/slurm
 ln -s "$GALAXY_DIR" "$SYMLINK_TARGET"
+ln -s "$SLURM_CONF_PATH" /etc/slurm-llnl/slurm.conf
 exec /usr/local/bin/supervisord -n -c /etc/supervisor/supervisord.conf
-
