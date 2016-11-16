266f0a91253dd4a3a4ba516248ff575126f93b8e
Lukas Voegtle <voegtlel@tf.uni-freiburg.de>
2016-11-16 14:25:15 +0100

    proftpd generates its config using ansible during startup changed compose to use the official postgres container as database added database initialization script to the galaxy container

---
+++ compose/proftpd/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Configurable variables via env
+playbookvars=('proftpd_db_connection' \
+'proftpd_db_username' \
+'proftpd_db_password' \
+'proftpd_sql_auth_type' \
+'proftpd_welcome' \
+'proftpd_passive_port_low' \
+'proftpd_passive_port_high' \
+'proftpd_use_sftp' \
+'proftpd_nat_masquerade' \
+'use_pbkdf2')
+
+# Generate override argument for ansible playbook
+playbookargs=""
+for var in "${playbookvars[@]}"
+do
+  if ! [ -z ${!var+x} ]
+  then
+    playbookargs="$playbookargs --extra-vars $var=\"${!var}\""
+    echo "Overriding $var=${!var}"
+  fi
+done
+
+# Run playbook to generate config, etc.
+ansible-playbook /ansible/provision.yml \
+    --tags=proftpd -c local $playbookargs
+# Run service
+proftpd --nodaemon
