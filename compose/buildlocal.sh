a56769c4d12b30fac306c340c96e1c07429f95cc
Lukas Voegtle <voegtlel@tf.uni-freiburg.de>
2016-12-23 19:46:45 +0100

    Added slurm container for compose

--- compose/buildlocal.sh
+++ compose/buildlocal.sh
@@ -6,3 +6,5 @@ docker build -t quay.io/galaxy/proftpd ./proftpd
 docker tag quay.io/galaxy/proftpd quay.io/galaxy/proftpd:compose
 docker build -t quay.io/galaxy/postgres ./postgres-galaxy
 docker tag quay.io/galaxy/postgres quay.io/galaxy/postgres:compose
+docker build -t quay.io/galaxy/slurm ./slurm-galaxy
+docker tag quay.io/galaxy/slurm quay.io/galaxy/slurm:compose
