ce1d4f9015fe04ed4898223b21608d462a742d6a
Bjoern Gruening <bjoern.gruening@gmail.com>
2017-06-11 19:03:54 +0200

    Sigularity testing in SLURM

--- compose/buildlocal.sh
+++ compose/buildlocal.sh
@@ -2,10 +2,10 @@
 set -x -e
 
 ANSIBLE_REPO=galaxyproject/ansible-galaxy-extras
-ANSIBLE_RELEASE=master
+ANSIBLE_RELEASE=singularity
 
-GALAXY_RELEASE=dev
-GALAXY_REPO=galaxyproject/galaxy
+GALAXY_RELEASE=singularity_mulled
+GALAXY_REPO=jmchilton/galaxy
 
 docker build --build-arg ANSIBLE_REPO=$ANSIBLE_REPO --build-arg ANSIBLE_RELEASE=$ANSIBLE_RELEASE -t quay.io/bgruening/galaxy-base ./galaxy-base/
 docker build --build-arg GALAXY_REPO=$GALAXY_REPO --build-arg GALAXY_RELEASE=$GALAXY_RELEASE -t quay.io/bgruening/galaxy-init ./galaxy-init/
