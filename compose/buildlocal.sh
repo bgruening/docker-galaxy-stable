b6429db868486e65827adb4a761acc3a8aab8d27
Pablo Moreno <pablo.a.moreno@gmail.com>
2017-11-21 10:00:54 +0000

    Uses :dev tag for FROMs in all relevant containers.

--- compose/buildlocal.sh
+++ compose/buildlocal.sh
@@ -9,15 +9,18 @@ GALAXY_REPO=galaxyproject/galaxy
 
 DOCKER_ADDITIONAL_BUILD_ARGS="--no-cache"
 
+# For using latest simply leave this variable empty or set to ":latest". This should be the case on the master branch.
+TAG=":dev"
+
 docker pull postgres
 
-docker build $DOCKER_ADDITIONAL_BUILD_ARGS --build-arg ANSIBLE_REPO=$ANSIBLE_REPO --build-arg ANSIBLE_RELEASE=$ANSIBLE_RELEASE -t quay.io/bgruening/galaxy-base ./galaxy-base/
-docker build $DOCKER_ADDITIONAL_BUILD_ARGS --build-arg GALAXY_REPO=$GALAXY_REPO --build-arg GALAXY_RELEASE=$GALAXY_RELEASE -t quay.io/bgruening/galaxy-init ./galaxy-init/
+docker build $DOCKER_ADDITIONAL_BUILD_ARGS --build-arg ANSIBLE_REPO=$ANSIBLE_REPO --build-arg ANSIBLE_RELEASE=$ANSIBLE_RELEASE -t quay.io/bgruening/galaxy-base$TAG ./galaxy-base/
+docker build $DOCKER_ADDITIONAL_BUILD_ARGS --build-arg GALAXY_REPO=$GALAXY_REPO --build-arg GALAXY_RELEASE=$GALAXY_RELEASE -t quay.io/bgruening/galaxy-init$TAG ./galaxy-init/
 
 # Build the Galaxy web-application container
-docker build $DOCKER_ADDITIONAL_BUILD_ARGS -t quay.io/bgruening/galaxy-web ./galaxy-web/
+docker build $DOCKER_ADDITIONAL_BUILD_ARGS -t quay.io/bgruening/galaxy-web$TAG ./galaxy-web/
 
-docker build $DOCKER_ADDITIONAL_BUILD_ARGS --build-arg ANSIBLE_REPO=$ANSIBLE_REPO --build-arg ANSIBLE_RELEASE=$ANSIBLE_RELEASE -t quay.io/galaxy/proftpd ./galaxy-proftpd
+docker build $DOCKER_ADDITIONAL_BUILD_ARGS --build-arg ANSIBLE_REPO=$ANSIBLE_REPO --build-arg ANSIBLE_RELEASE=$ANSIBLE_RELEASE -t quay.io/galaxy/proftpd$TAG ./galaxy-proftpd
 
 # Build the postgres container
 docker build -t quay.io/galaxy/postgres ./galaxy-postgres
@@ -26,7 +29,7 @@ docker build -t quay.io/galaxy/postgres ./galaxy-postgres
 docker build -t quay.io/galaxy/slurm ./galaxy-slurm
 
 # we build a common HTCondor and derive from that laster
-docker build -t quay.io/bgruening/galaxy-htcondor-base ./galaxy-htcondor-base
-docker build -t quay.io/bgruening/galaxy-htcondor ./galaxy-htcondor
-docker build -t quay.io/bgruening/galaxy-htcondor-executor ./galaxy-htcondor-executor
+docker build -t quay.io/bgruening/galaxy-htcondor-base$TAG ./galaxy-htcondor-base
+docker build -t quay.io/bgruening/galaxy-htcondor$TAG ./galaxy-htcondor
+docker build -t quay.io/bgruening/galaxy-htcondor-executor$TAG ./galaxy-htcondor-executor
 
