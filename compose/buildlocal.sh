34d8081900419caff1fafd00b264b3e590677f71
Bjoern Gruening <bjoern.gruening@gmail.com>
2018-03-04 22:44:30 +0100

    add dev tag

--- compose/buildlocal.sh
+++ compose/buildlocal.sh
@@ -11,7 +11,7 @@ DOCKER_ADDITIONAL_BUILD_ARGS=""
 #"--no-cache"
 
 # For using latest simply leave this variable empty or set to ":latest". This should be the case on the master branch.
-export TAG=":dev"
+TAG=":dev"
 
 docker pull postgres
 
