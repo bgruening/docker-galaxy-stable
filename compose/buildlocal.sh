b26c41386206bae08e9395c6bd7703f589999277
Bjoern Gruening <bjoern.gruening@gmail.com>
2018-05-05 14:59:53 +0200

    fix some issues found via NFS-Swarm deployment

--- compose/buildlocal.sh
+++ compose/buildlocal.sh
@@ -4,11 +4,10 @@ set -x -e
 ANSIBLE_REPO=galaxyproject/ansible-galaxy-extras
 ANSIBLE_RELEASE=18.01
 
-GALAXY_RELEASE=dev
+GALAXY_RELEASE=release_18.01
 GALAXY_REPO=galaxyproject/galaxy
 
-DOCKER_ADDITIONAL_BUILD_ARGS=""
-#"--no-cache"
+DOCKER_ADDITIONAL_BUILD_ARGS="--no-cache"
 
 # For using latest simply leave this variable empty or set to ":latest". This should be the case on the master branch.
 TAG=":dev"
@@ -21,13 +20,13 @@ docker build $DOCKER_ADDITIONAL_BUILD_ARGS --build-arg GALAXY_REPO=$GALAXY_REPO 
 # Build the Galaxy web-application container
 docker build $DOCKER_ADDITIONAL_BUILD_ARGS -t quay.io/bgruening/galaxy-web$TAG ./galaxy-web/
 
-docker build $DOCKER_ADDITIONAL_BUILD_ARGS --build-arg ANSIBLE_REPO=$ANSIBLE_REPO --build-arg ANSIBLE_RELEASE=$ANSIBLE_RELEASE -t quay.io/galaxy/proftpd$TAG ./galaxy-proftpd
+docker build $DOCKER_ADDITIONAL_BUILD_ARGS --build-arg ANSIBLE_REPO=$ANSIBLE_REPO --build-arg ANSIBLE_RELEASE=$ANSIBLE_RELEASE -t quay.io/bgruening/galaxy-proftpd$TAG ./galaxy-proftpd
 
 # Build the postgres container
-docker build $DOCKER_ADDITIONAL_BUILD_ARGS -t quay.io/galaxy/postgres$TAG ./galaxy-postgres
+docker build $DOCKER_ADDITIONAL_BUILD_ARGS -t quay.io/bgruening/galaxy-postgres$TAG ./galaxy-postgres
 
 # The SLURM cluster
-docker build $DOCKER_ADDITIONAL_BUILD_ARGS -t quay.io/galaxy/slurm$TAG ./galaxy-slurm
+docker build $DOCKER_ADDITIONAL_BUILD_ARGS -t quay.io/bgruening/galaxy-slurm$TAG ./galaxy-slurm
 
 # we build a common HTCondor and derive from that laster
 docker build -t quay.io/bgruening/galaxy-htcondor-base$TAG ./galaxy-htcondor-base
