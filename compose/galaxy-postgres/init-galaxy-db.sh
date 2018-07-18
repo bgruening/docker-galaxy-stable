a2e3b69c54e5100e930f33a2b9851be82765c4a0
Luca Pireddu <pireddu@crs4.it>
2018-07-18 19:24:08 +0200

    Remove the Galaxy schema from the PostgreSQL image
    This commit effectively turns the galaxy-postgres image into a retagging of
    the standard PostgreSQL image.

--- compose/galaxy-postgres/init-galaxy-db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Initializing galaxy database with defaults"
-"${psql[@]}" < /docker-entrypoint-initdb.d/init-galaxy-db.sql.in >/dev/null
-echo "Successfully initialized galaxy database"
