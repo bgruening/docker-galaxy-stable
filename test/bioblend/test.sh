1fe05a6b1619e41bdd0e339f90ed4abf711b073d
Bjoern Gruening <bjoern.gruening@gmail.com>
2018-11-03 16:32:18 +0100

    tox changes

--- test/bioblend/test.sh
+++ test/bioblend/test.sh
@@ -2,17 +2,16 @@
 if [ "${COMPOSE_SLURM}" ] || [ "${KUBE}" ] || [ "${COMPOSE_CONDOR_DOCKER}" ] || [ "${COMPOSE_SLURM_SINGULARITY}" ]
 then
     docker_exec bash -c 'cd /home/galaxy ;
-    . /galaxy_venv/bin/activate ;
+    export PATH=/tool_deps/_conda/bin/:$PATH; . activate galaxy_env;
     wget https://github.com/galaxyproject/bioblend/archive/master.tar.gz && tar xfz master.tar.gz ;
     cd bioblend-master ;
     pip install --upgrade "tox>=1.8.0" "pep8<=1.6.2" ;
     python setup.py install ;
     sed -i.bak "s/commands.*$/commands =/" tox.ini ;
-    export TOX_ENV=py27 ;
     export BIOBLEND_GALAXY_API_KEY=admin ;
     export BIOBLEND_GALAXY_URL=http://galaxy ;
     cd /home/galaxy/bioblend-master ;
-    tox -e $TOX_ENV -- -e "test_download_dataset|test_upload_from_galaxy_filesystem|test_get_datasets|test_datasets_from_fs|test_existing_history|test_new_history|test_params|test_tool_dependency_install|test_download_history|test_export_and_download"'
+    tox -e "test_download_dataset|test_upload_from_galaxy_filesystem|test_get_datasets|test_datasets_from_fs|test_existing_history|test_new_history|test_params|test_tool_dependency_install|test_download_history|test_export_and_download"'
 
 else
     docker build -t bioblend_test .
