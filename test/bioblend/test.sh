87de476982986b99e63bc5ebe67a294345409231
Bjoern Gruening <bjoern.gruening@gmail.com>
2018-11-03 16:38:52 +0100

    install virtualenv via conda

--- test/bioblend/test.sh
+++ test/bioblend/test.sh
@@ -8,10 +8,11 @@ then
     pip install --upgrade "tox>=1.8.0" "pep8<=1.6.2" ;
     python setup.py install ;
     sed -i.bak "s/commands.*$/commands =/" tox.ini ;
+    export TOX_ENV=py27 ;
     export BIOBLEND_GALAXY_API_KEY=admin ;
     export BIOBLEND_GALAXY_URL=http://galaxy ;
     cd /home/galaxy/bioblend-master ;
-    tox -e "test_download_dataset|test_upload_from_galaxy_filesystem|test_get_datasets|test_datasets_from_fs|test_existing_history|test_new_history|test_params|test_tool_dependency_install|test_download_history|test_export_and_download"'
+    tox -e $TOX_ENV -- -e "test_download_dataset|test_upload_from_galaxy_filesystem|test_get_datasets|test_datasets_from_fs|test_existing_history|test_new_history|test_params|test_tool_dependency_install|test_download_history|test_export_and_download"'
 
 else
     docker build -t bioblend_test .
