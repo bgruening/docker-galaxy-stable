f9afda02e03a4f15fd4a0cb90af93ed34af4be1e
Bjoern Gruening <bjoern.gruening@gmail.com>
2019-03-05 09:57:36 +0100

    update DB init

--- compose/galaxy-postgres/init-galaxy-db.sh
+++ compose/galaxy-postgres/init-galaxy-db.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 echo "Initializing galaxy database with defaults"
-"${psql[@]}" < /docker-entrypoint-initdb.d/init-galaxy-db.sql.in >/dev/null
+
+psql -v ON_ERROR_STOP=1 --username ${POSTGRES_USER:-galaxy} --dbname ${POSTGRES_DB:-galaxy} <<- EOSQL
+    CREATE USER ${POSTGRES_USER:-galaxy};
+    CREATE DATABASE ${POSTGRES_DB:-${POSTGRES_USER:-galaxy}};
+    GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB:-galaxy} TO ${POSTGRES_USER:-galaxy};
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username ${POSTGRES_USER:-galaxy} --dbname ${POSTGRES_DB:-galaxy} < /docker-entrypoint-initdb.d/init-galaxy-db.sql.in
+
 echo "Successfully initialized galaxy database"
