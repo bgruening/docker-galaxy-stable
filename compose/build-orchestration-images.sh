845c61537baac10eb4306cdcd32259d3c6710452
Gianmauro Cuccuru <gmauro@gmail.com>
2019-05-06 17:43:11 +0200

    update compose to track release_19.05

--- compose/build-orchestration-images.sh
+++ compose/build-orchestration-images.sh
@@ -131,7 +131,7 @@ DOCKER_USER=${CONTAINER_USER:-bgruening}
 ANSIBLE_REPO=${ANSIBLE_REPO:-galaxyproject/ansible-galaxy-extras}
 ANSIBLE_RELEASE=${ANSIBLE_RELEASE:-19.05}
 
-GALAXY_VERSION=${GALAXY_VERSION:-dev}
+GALAXY_VERSION=${GALAXY_VERSION:-release_19.05}
 
 # GALAXY_BASE_FROM_TO_REPLACE=${GALAXY_BASE_FROM_TO_REPLACE:-quay.io/bgruening/galaxy-base:$GALAXY_VERSION}
 GALAXY_BASE_FROM_TO_REPLACE=$(grep ^FROM galaxy-init/Dockerfile | awk '{ print $2 }') # init starts from base, so we get it from there.
