0ee74e2e64646d3dbc6481b0a3207e8a820d5c09
Andreas Skorczyk <me@andreas-sk.de>
2019-12-24 18:07:39 +0100

    Start script: Wait for RabbitMQ & start directly using uwsgi

--- compose-v2/galaxy-server/files/start.sh
+++ compose-v2/galaxy-server/files/start.sh
@@ -6,16 +6,18 @@ if [ ! -d  "$EXPORT_DIR/$GALAXY_ROOT" ]; then
     mkdir $EXPORT_DIR/$GALAXY_ROOT
 fi
 
-if [ ! -d  "$EXPORT_DIR/$GALAXY_CONFIG_DIR" ]; then
+if [ ! -d  "$EXPORT_DIR/$GALAXY_CONFIG_DIR" ] || [ -z "$(ls -A $EXPORT_DIR/$GALAXY_CONFIG_DIR | grep -L plugins)" ]; then
     # Move config to $EXPORT_DIR and create symlink 
-    mv -v $GALAXY_CONFIG_DIR $EXPORT_DIR/$GALAXY_CONFIG_DIR
-    ls /
+    mv -v $GALAXY_CONFIG_DIR/* $EXPORT_DIR/$GALAXY_CONFIG_DIR
+    mv -v $GALAXY_CONFIG_DIR/plugins/* $EXPORT_DIR/$GALAXY_CONFIG_DIR/plugins
+    rm -rf $GALAXY_CONFIG_DIR
     ln -v -s $EXPORT_DIR/$GALAXY_CONFIG_DIR $GALAXY_CONFIG_DIR
 fi
 
 if [ ! -d  "$EXPORT_DIR/$GALAXY_STATIC_DIR" ] || [ -z "$(ls -A $EXPORT_DIR/$GALAXY_STATIC_DIR)" ]; then
     # Move static to $EXPORT_DIR and create symlink
     mv -v $GALAXY_STATIC_DIR/* $EXPORT_DIR/$GALAXY_STATIC_DIR
+    rm -rf $GALAXY_STATIC_DIR
     ln -v -s $EXPORT_DIR/$GALAXY_STATIC_DIR $GALAXY_STATIC_DIR
 fi
 
@@ -32,13 +34,22 @@ if [ ! -d  "$EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR" ]; then
  fi   
 echo "Finished initialization"
 
-# Not first start:
-    # Start Galaxy
+echo "Waiting for RabbitMQ..."
+until nc -z -w 2 rabbitmq 5672 && echo RabbitMQ started; do
+    sleep 1;
+done;
 
 echo "Waiting for Postgres..."
 until nc -z -w 2 postgres 5432 && echo Postgres started; do
     sleep 1;
 done;
 
+# Install additional dependency: psycopg2
+. $GALAXY_ROOT/.venv/bin/activate
+pip install psycopg2
+deactivate
+
 echo "Starting Galaxy now.."
-$GALAXY_ROOT/run.sh
+cd $GALAXY_ROOT
+$GALAXY_ROOT/.venv/bin/uwsgi --yaml $GALAXY_CONFIG_DIR/galaxy.yml --pythonpath $GALAXY_ROOT/lib --module 'galaxy.webapps.galaxy.buildapp:uwsgi_app()' --virtualenv /galaxy/.venv 
+# --uid $GALAXY_UID --gid $GALAXY_GID 
