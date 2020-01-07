d75e648646b1e472b7a1fb67c485f938a6d71662
Andreas Skorczyk <me@andreas-sk.de>
2020-01-07 11:42:10 +0100

    Run as Galaxy_User & fix permission problems

--- compose-v2/galaxy-server/files/start.sh
+++ compose-v2/galaxy-server/files/start.sh
@@ -1,4 +1,3 @@
-set +x
 # First start?? Check if something exists that indicates that environment is not new.. Config file? Something in DB maybe??
 
 echo "Initialization: Check if files already exist, export otherwise."
@@ -10,48 +9,60 @@ fi
 if [ ! -d  "$EXPORT_DIR/$GALAXY_CONFIG_DIR" ] || [ -z "$(ls -p $EXPORT_DIR/$GALAXY_CONFIG_DIR | grep -v /)" ]; then
     # Move config to $EXPORT_DIR and create symlink 
     mkdir $EXPORT_DIR/$GALAXY_CONFIG_DIR
-    cp -rfv $GALAXY_CONFIG_DIR/* $EXPORT_DIR/$GALAXY_CONFIG_DIR
-    cp -rfv $GALAXY_CONFIG_DIR/plugins/* $EXPORT_DIR/$GALAXY_CONFIG_DIR/plugins
+    chown $GALAXY_USER:$GALAXY_USER $EXPORT_DIR/$GALAXY_CONFIG_DIR
+    cp -rpf $GALAXY_CONFIG_DIR/* $EXPORT_DIR/$GALAXY_CONFIG_DIR
+    cp -rpf $GALAXY_CONFIG_DIR/plugins/* $EXPORT_DIR/$GALAXY_CONFIG_DIR/plugins
 fi
 rm -rf $GALAXY_CONFIG_DIR
 ln -v -s $EXPORT_DIR/$GALAXY_CONFIG_DIR $GALAXY_CONFIG_DIR
+chown -h $GALAXY_USER:$GALAXY_USER $GALAXY_CONFIG_DIR
 
 if [ ! -d  "$EXPORT_DIR/$GALAXY_STATIC_DIR" ] || [ -z "$(ls -A $EXPORT_DIR/$GALAXY_STATIC_DIR)" ]; then
     # Move static to $EXPORT_DIR and create symlink
     mkdir $EXPORT_DIR/$GALAXY_STATIC_DIR
-    cp -rfv $GALAXY_STATIC_DIR/* $EXPORT_DIR/$GALAXY_STATIC_DIR
+    chown $GALAXY_USER:$GALAXY_USER $EXPORT_DIR/$GALAXY_STATIC_DIR
+    cp -rpf $GALAXY_STATIC_DIR/* $EXPORT_DIR/$GALAXY_STATIC_DIR
 fi
 rm -rf $GALAXY_STATIC_DIR
 ln -v -s $EXPORT_DIR/$GALAXY_STATIC_DIR $GALAXY_STATIC_DIR
+chown -h $GALAXY_USER:$GALAXY_USER $GALAXY_STATIC_DIR
 
 if [ ! -d  "$EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH" ] || [ -z "$(ls -A $EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH)" ]; then
     # Move environment to export and create symlink
     mkdir $EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH
-    cp -rfv $GALAXY_CONFIG_TOOL_PATH/* $EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH
+    chown $GALAXY_USER:$GALAXY_USER $EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH
+    cp -rpf $GALAXY_CONFIG_TOOL_PATH/* $EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH
 fi
 rm -rf $GALAXY_CONFIG_TOOL_PATH
 ln -v -s $EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH $GALAXY_CONFIG_TOOL_PATH
+chown -h $GALAXY_USER:$GALAXY_USER $GALAXY_CONFIG_TOOL_PATH
 
 if [ ! -d  "$EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR" ] || [ -z "$(ls -A $EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR)" ]; then
     # Move tools and tool-deps to export and create symlink
     mkdir $EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR
-    cp -rfv $GALAXY_CONFIG_TOOL_DEPENDENCY_DIR/* $EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR
+    chown $GALAXY_USER:$GALAXY_USER $EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR
+    cp -rpf $GALAXY_CONFIG_TOOL_DEPENDENCY_DIR/* $EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR
 fi
 rm -rf $GALAXY_CONFIG_TOOL_DEPENDENCY_DIR
 ln -v -s $EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR $GALAXY_CONFIG_TOOL_DEPENDENCY_DIR
+chown -h $GALAXY_USER:$GALAXY_USER $GALAXY_CONFIG_TOOL_DEPENDENCY_DIR
 
 # Move Galaxy virtual environment
 if [ ! -d  "$EXPORT_DIR/$GALAXY_VIRTUAL_ENV" ] || [ -z "$(ls -A $EXPORT_DIR/$GALAXY_VIRTUAL_ENV)" ]; then
     mkdir $EXPORT_DIR/$GALAXY_VIRTUAL_ENV
-    cp -rf $GALAXY_VIRTUAL_ENV/* $EXPORT_DIR/$GALAXY_VIRTUAL_ENV
+    chown $GALAXY_USER:$GALAXY_USER $EXPORT_DIR/$GALAXY_VIRTUAL_ENV
+    cp -rpf $GALAXY_VIRTUAL_ENV/* $EXPORT_DIR/$GALAXY_VIRTUAL_ENV
 fi
 rm -rf $GALAXY_VIRTUAL_ENV
 ln -v -s $EXPORT_DIR/$GALAXY_VIRTUAL_ENV $GALAXY_VIRTUAL_ENV
+chown -h $GALAXY_USER:$GALAXY_USER $GALAXY_VIRTUAL_ENV
 
 # Export database-folder (used for job files etc)
 rm -rf $GALAXY_DATABASE_PATH
 mkdir $EXPORT_DIR/$GALAXY_DATABASE_PATH
+chown $GALAXY_USER:$GALAXY_USER $EXPORT_DIR/$GALAXY_DATABASE_PATH
 ln -v -s $EXPORT_DIR/$GALAXY_DATABASE_PATH $GALAXY_DATABASE_PATH
+chown -h $GALAXY_USER:$GALAXY_USER $GALAXY_DATABASE_PATH
 
 echo "Finished initialization"
 
@@ -68,5 +79,4 @@ done;
 
 echo "Starting Galaxy now.."
 cd $GALAXY_ROOT
-$GALAXY_ROOT/.venv/bin/uwsgi --yaml $GALAXY_CONFIG_DIR/galaxy.yml --pythonpath $GALAXY_ROOT/lib --module 'galaxy.webapps.galaxy.buildapp:uwsgi_app()' --virtualenv /galaxy/.venv 
-# --uid $GALAXY_UID --gid $GALAXY_GID 
+$GALAXY_ROOT/.venv/bin/uwsgi --yaml $GALAXY_CONFIG_DIR/galaxy.yml --pythonpath $GALAXY_ROOT/lib --module 'galaxy.webapps.galaxy.buildapp:uwsgi_app()' --virtualenv /galaxy/.venv --uid $GALAXY_UID --gid $GALAXY_GID 
