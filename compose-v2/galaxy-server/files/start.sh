2f8536cbed7f6de6b1cb37211a514cf0ae2f05e4
Andreas Skorczyk <me@andreas-sk.de>
2020-01-07 13:51:48 +0100

    Lint: fix shellcheck warnings in Galaxy start script

--- compose-v2/galaxy-server/files/start.sh
+++ compose-v2/galaxy-server/files/start.sh
@@ -1,68 +1,75 @@
+#!/bin/sh
+
 # First start?? Check if something exists that indicates that environment is not new.. Config file? Something in DB maybe??
 
 echo "Initialization: Check if files already exist, export otherwise."
 if [ ! -d  "$EXPORT_DIR/$GALAXY_ROOT" ]; then
     # Create initial $GALAXY_ROOT in $EXPORT_DIR
-    mkdir $EXPORT_DIR/$GALAXY_ROOT
+    mkdir "$EXPORT_DIR/$GALAXY_ROOT"
 fi
 
+# shellcheck disable=SC2143,SC2086,SC2010
 if [ ! -d  "$EXPORT_DIR/$GALAXY_CONFIG_DIR" ] || [ -z "$(ls -p $EXPORT_DIR/$GALAXY_CONFIG_DIR | grep -v /)" ]; then
     # Move config to $EXPORT_DIR and create symlink 
-    mkdir $EXPORT_DIR/$GALAXY_CONFIG_DIR
-    chown $GALAXY_USER:$GALAXY_USER $EXPORT_DIR/$GALAXY_CONFIG_DIR
-    cp -rpf $GALAXY_CONFIG_DIR/* $EXPORT_DIR/$GALAXY_CONFIG_DIR
-    cp -rpf $GALAXY_CONFIG_DIR/plugins/* $EXPORT_DIR/$GALAXY_CONFIG_DIR/plugins
+    mkdir "$EXPORT_DIR/$GALAXY_CONFIG_DIR"
+    chown "$GALAXY_USER:$GALAXY_USER" "$EXPORT_DIR/$GALAXY_CONFIG_DIR"
+    cp -rpf "$GALAXY_CONFIG_DIR/*" "$EXPORT_DIR/$GALAXY_CONFIG_DIR"
+    cp -rpf "$GALAXY_CONFIG_DIR/plugins/*" "$EXPORT_DIR/$GALAXY_CONFIG_DIR/plugins"
 fi
-rm -rf $GALAXY_CONFIG_DIR
-ln -v -s $EXPORT_DIR/$GALAXY_CONFIG_DIR $GALAXY_CONFIG_DIR
-chown -h $GALAXY_USER:$GALAXY_USER $GALAXY_CONFIG_DIR
+rm -rf "$GALAXY_CONFIG_DIR"
+ln -v -s "$EXPORT_DIR/$GALAXY_CONFIG_DIR" "$GALAXY_CONFIG_DIR"
+chown -h "$GALAXY_USER:$GALAXY_USER" "$GALAXY_CONFIG_DIR"
 
+# shellcheck disable=SC2143,SC2086,SC2010
 if [ ! -d  "$EXPORT_DIR/$GALAXY_STATIC_DIR" ] || [ -z "$(ls -A $EXPORT_DIR/$GALAXY_STATIC_DIR)" ]; then
     # Move static to $EXPORT_DIR and create symlink
-    mkdir $EXPORT_DIR/$GALAXY_STATIC_DIR
-    chown $GALAXY_USER:$GALAXY_USER $EXPORT_DIR/$GALAXY_STATIC_DIR
-    cp -rpf $GALAXY_STATIC_DIR/* $EXPORT_DIR/$GALAXY_STATIC_DIR
+    mkdir "$EXPORT_DIR/$GALAXY_STATIC_DIR"
+    chown "$GALAXY_USER:$GALAXY_USER" "$EXPORT_DIR/$GALAXY_STATIC_DIR"
+    cp -rpf "$GALAXY_STATIC_DIR/*" "$EXPORT_DIR/$GALAXY_STATIC_DIR"
 fi
-rm -rf $GALAXY_STATIC_DIR
-ln -v -s $EXPORT_DIR/$GALAXY_STATIC_DIR $GALAXY_STATIC_DIR
-chown -h $GALAXY_USER:$GALAXY_USER $GALAXY_STATIC_DIR
+rm -rf "$GALAXY_STATIC_DIR"
+ln -v -s "$EXPORT_DIR/$GALAXY_STATIC_DIR" "$GALAXY_STATIC_DIR"
+chown -h "$GALAXY_USER:$GALAXY_USER" "$GALAXY_STATIC_DIR"
 
+# shellcheck disable=SC2143,SC2086,SC2010
 if [ ! -d  "$EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH" ] || [ -z "$(ls -A $EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH)" ]; then
     # Move environment to export and create symlink
-    mkdir $EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH
-    chown $GALAXY_USER:$GALAXY_USER $EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH
-    cp -rpf $GALAXY_CONFIG_TOOL_PATH/* $EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH
+    mkdir "$EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH"
+    chown "$GALAXY_USER:$GALAXY_USER" "$EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH"
+    cp -rpf "$GALAXY_CONFIG_TOOL_PATH/*" "$EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH"
 fi
-rm -rf $GALAXY_CONFIG_TOOL_PATH
-ln -v -s $EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH $GALAXY_CONFIG_TOOL_PATH
-chown -h $GALAXY_USER:$GALAXY_USER $GALAXY_CONFIG_TOOL_PATH
+rm -rf "$GALAXY_CONFIG_TOOL_PATH"
+ln -v -s "$EXPORT_DIR/$GALAXY_CONFIG_TOOL_PATH" "$GALAXY_CONFIG_TOOL_PATH"
+chown -h "$GALAXY_USER:$GALAXY_USER" "$GALAXY_CONFIG_TOOL_PATH"
 
+# shellcheck disable=SC2143,SC2086,SC2010
 if [ ! -d  "$EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR" ] || [ -z "$(ls -A $EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR)" ]; then
     # Move tools and tool-deps to export and create symlink
-    mkdir $EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR
-    chown $GALAXY_USER:$GALAXY_USER $EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR
-    cp -rpf $GALAXY_CONFIG_TOOL_DEPENDENCY_DIR/* $EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR
+    mkdir "$EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR"
+    chown "$GALAXY_USER:$GALAXY_USER" "$EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR"
+    cp -rpf "$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR/*" "$EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR"
 fi
-rm -rf $GALAXY_CONFIG_TOOL_DEPENDENCY_DIR
-ln -v -s $EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR $GALAXY_CONFIG_TOOL_DEPENDENCY_DIR
-chown -h $GALAXY_USER:$GALAXY_USER $GALAXY_CONFIG_TOOL_DEPENDENCY_DIR
+rm -rf "$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR"
+ln -v -s "$EXPORT_DIR/$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR" "$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR"
+chown -h "$GALAXY_USER:$GALAXY_USER" "$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR"
 
 # Move Galaxy virtual environment
+# shellcheck disable=SC2143,SC2086,SC2010
 if [ ! -d  "$EXPORT_DIR/$GALAXY_VIRTUAL_ENV" ] || [ -z "$(ls -A $EXPORT_DIR/$GALAXY_VIRTUAL_ENV)" ]; then
-    mkdir $EXPORT_DIR/$GALAXY_VIRTUAL_ENV
-    chown $GALAXY_USER:$GALAXY_USER $EXPORT_DIR/$GALAXY_VIRTUAL_ENV
-    cp -rpf $GALAXY_VIRTUAL_ENV/* $EXPORT_DIR/$GALAXY_VIRTUAL_ENV
+    mkdir "$EXPORT_DIR/$GALAXY_VIRTUAL_ENV"
+    chown "$GALAXY_USER:$GALAXY_USER" "$EXPORT_DIR/$GALAXY_VIRTUAL_ENV"
+    cp -rpf "$GALAXY_VIRTUAL_ENV/*" "$EXPORT_DIR/$GALAXY_VIRTUAL_ENV"
 fi
-rm -rf $GALAXY_VIRTUAL_ENV
-ln -v -s $EXPORT_DIR/$GALAXY_VIRTUAL_ENV $GALAXY_VIRTUAL_ENV
-chown -h $GALAXY_USER:$GALAXY_USER $GALAXY_VIRTUAL_ENV
+rm -rf "$GALAXY_VIRTUAL_ENV"
+ln -v -s "$EXPORT_DIR/$GALAXY_VIRTUAL_ENV" "$GALAXY_VIRTUAL_ENV"
+chown -h "$GALAXY_USER:$GALAXY_USER" "$GALAXY_VIRTUAL_ENV"
 
 # Export database-folder (used for job files etc)
-rm -rf $GALAXY_DATABASE_PATH
-mkdir $EXPORT_DIR/$GALAXY_DATABASE_PATH
-chown $GALAXY_USER:$GALAXY_USER $EXPORT_DIR/$GALAXY_DATABASE_PATH
-ln -v -s $EXPORT_DIR/$GALAXY_DATABASE_PATH $GALAXY_DATABASE_PATH
-chown -h $GALAXY_USER:$GALAXY_USER $GALAXY_DATABASE_PATH
+rm -rf "$GALAXY_DATABASE_PATH"
+mkdir "$EXPORT_DIR/$GALAXY_DATABASE_PATH"
+chown "$GALAXY_USER:$GALAXY_USER" "$EXPORT_DIR/$GALAXY_DATABASE_PATH"
+ln -v -s "$EXPORT_DIR/$GALAXY_DATABASE_PATH" "$GALAXY_DATABASE_PATH"
+chown -h "$GALAXY_USER:$GALAXY_USER" "$GALAXY_DATABASE_PATH"
 
 echo "Finished initialization"
 
@@ -76,7 +83,16 @@ until nc -z -w 2 postgres 5432 && echo Postgres started; do
     sleep 1;
 done;
 
+if [ -f "/etc/condor/condor_config.local" ]; then
+    echo "HTCondor config file found"
+    echo "Copying Galaxy library to /export (needed by HTCondor workers).."
+    mkdir "$EXPORT_DIR/$GALAXY_ROOT/lib"
+    chown "$GALAXY_USER:$GALAXY_USER" "$EXPORT_DIR/$GALAXY_ROOT/lib"
+    cp -rpf "$GALAXY_ROOT/lib/*" "$EXPORT_DIR/$GALAXY_ROOT/lib"
+    echo "Starting HTCondor.."
+    service condor start
+fi
 
 echo "Starting Galaxy now.."
-cd $GALAXY_ROOT
-$GALAXY_ROOT/.venv/bin/uwsgi --yaml $GALAXY_CONFIG_DIR/galaxy.yml --pythonpath $GALAXY_ROOT/lib --module 'galaxy.webapps.galaxy.buildapp:uwsgi_app()' --virtualenv /galaxy/.venv --uid $GALAXY_UID --gid $GALAXY_GID 
+cd "$GALAXY_ROOT" || { echo "Error: Could not change to $GALAXY_ROOT"; exit 1; }
+"$GALAXY_ROOT/.venv/bin/uwsgi" --yaml "$GALAXY_CONFIG_DIR/galaxy.yml" --pythonpath "$GALAXY_ROOT/lib" --module "galaxy.webapps.galaxy.buildapp:uwsgi_app()" --virtualenv /galaxy/.venv --uid "$GALAXY_UID" --gid "$GALAXY_GID"
