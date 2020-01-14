b0a7117d9d5a96f625434db0328e016c82f14854
Andreas Skorczyk <me@andreas-sk.de>
2020-01-14 09:13:37 +0100

    Configurator: Wait for Galaxy to populate config first

--- compose-v2/galaxy-configurator/run.sh
+++ compose-v2/galaxy-configurator/run.sh
@@ -7,6 +7,11 @@ fi
 
 cd ${GALAXY_CONFIG_DIR:-/galaxy/config} || { echo "Error: Could not find Galaxy config dir"; exit 1; }
 
+echo "Waiting for Galaxy config dir to be initially populated (in case of first startup)"
+until [ "$(ls -p | grep -v /)" != "" ] && echo Galaxy config populated; do
+    sleep 0.5;
+done;
+
 echo "Configuring job_conf.xml"
 j2 --undefined -o /galaxy/config/job_conf.xml /templates/job_conf.xml.j2
 
