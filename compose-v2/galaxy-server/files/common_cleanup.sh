ee7f98d947f6fb8c76d0a9329968443f857a6dc3
Andreas Skorczyk <me@andreas-sk.de>
2020-05-22 10:46:31 +0200

    Small comment for common_cleanup.sh
    Make clear that deleting Python cache file
    reduces container size at the cost of startup
    time, as suggested in
    https://github.com/bgruening/docker-galaxy-stable/pull/546#discussion_r376772333

--- compose-v2/galaxy-server/files/common_cleanup.sh
+++ compose-v2/galaxy-server/files/common_cleanup.sh
@@ -2,7 +2,10 @@
 
 set -x
 
+# This usually drastically reduced the container size
+# at the cost of the startup time of your application
 find / -name '*.pyc' -delete
+
 find / -name '*.log' -delete
 find / -name '.cache' -delete
 rm -rf /var/lib/apt/lists/*
